4b649752b8c4a18d721aee21eb37cab615b05d7a
microwave89-hv <58249032+microwave89-hv@users.noreply.github.com>
2020-01-12 21:04:27 +0100

    Update main.asm

--- main.asm
+++ main.asm
@@ -33,36 +33,33 @@ Subsystem: 				db IMAGE_SUBSYSTEM_EFI_APPLICATION 	; 0A
 StrippedSize: 				dw 0 					; 00 00 		  ; Not needed ==> EntryPoint = AddressOfEntryPoint + sizeof(EFI_TE_IMAGE_HEADER);
 AddressOfEntryPoint: 			dd (efi_miau - size_of_te_hdr)		; 00 		  	  ; EntryPoint is "efi_miau"
 BaseOfCode:
-	push 0 									; 6A 00			  ; unsigned long long var4 = 0;
+	push EfiResetShutdown	 						; 6A 02			  ; unsigned long long var1 = (unsigned long long)EfiResetShutdown;
 	jmp DataDirectory0_Va 							; EB 08 		  ; Skip non-executable ImageBase
 
-ImageBase: 				dq 0x1000 				; 00 10 00 00 00 00 00 00 ; This region is usable on my system.
+ImageBase: 				dq 0x0000000000001000	 		; 00 10 00 00 05 60 00 00 ; On my system this region is read-writeable. This might not hold true with your's.
 DataDirectory0_Va:
-	push 0 									; 6A 00			  ; unsigned long long var3 = 0;
-	jmp DataDirectory1_Va 							; EB 04
+	pop rcx 								; 59			  ; arg1 = var1;
+	nop									; 90
+	jmp DataDirectory1_Va 							; EB 04			  ; Skip required data fields in execution flow
 
-DataDirectory0_Size: 			dd 0 					; 00 00 00 00 		  ; If directory sizes are 0 it appears that the VA's allow for arbitrary values.
+DataDirectory0_Size: 			dd 0 ;xfedcba97 				; 00 00 00 00 		  ; If directory sizes are 0 it appears that the VA's allow for arbitrary values.
 DataDirectory1_Va:
-	push 0 									; 6A 00			  ; unsigned long long var2 = 0;
-	jmp goAhead								; EB 06 		  ; Skip needed fields and the init jmp
+	xor edx, edx 								; 31 D2			  ; arg2 = EFI_SUCCESS;
+	jmp goAhead								; EB 0A 		  ; Skip required data fields, and skip instructions which have already been executed.
 
 DataDirectory1_Size: 			dd 0 					; 00 00 00 00
-
 efi_miau:									; EntryPoint
-	jmp BaseOfCode 								; EB E2 		  ; Go to very first usable byte, whose address is lower than anything
+	mov rax, [rdx + 0x58]							; 			  ; Firstly, save EFI_RUNTIME_SERVICES table pointer so rdx can already be zeroed.
+	jmp BaseOfCode	 							; EB DE 		  ; Go to very first usable byte, whose address is lower than anything
 													  ; AddressOfEntryPoint would allow for.
 goAhead:
-	push EfiResetShutdown 							; 6A 02 		  ; unsigned long long var1 = (unsigned long long)EfiResetShutdown;
-	mov rax, [rdx + 0x58] 							; 48 8B 42 58 		  ; EFI_RUNTIME_SERVICES* pRuntimeServices = pEfiSystemTable->pRuntimeServices;
-	pop rcx 								; 59 			  ; Retrieve the values which have been stored on the stack 
-	pop rdx 								; 5A 			  ; Pushing imm8 and following up by popping target registers requires less	
-	pop r8 									; 41 58 		  ; machine code than xoring 3 registers and pre-loading one with imm8.
-	pop r9 									; 41 59 		  ; Even more important so, loading registers by pushing to and popping values
-													  ; from the stack reduces the size of the instructions needed.
-													  ; With a size of just 2 bytes per instruction unused dword fields can be
-													  ; made use of by allowing for a connecting jmp in the remaining 2 bytes.
-	jmp [rax + 0x68]							; FF 60 68 		  ; pRuntimeServices->fpResetSystem(var1, var2, var3, var4);
+	jmp [rax + 0x68]							; FF 60 68 		  ; pRuntimeServices->fpResetSystem(arg1, arg2, arg3, arg4); Although the reset
+													  ; call has 4 parameters only 2 arguments are supplied. The 2nd parameter allows
+													  ; for providing a shutdown reason in terms of an EFI_STATUS. Implementers of
+													  ; the EFI_RESET_SYSTEM IF might use this information for logging reasons or such.
+													  ; According to the EFI spec, if the shutdown reason is EFI_SUCCESS the last 2
+													  ; arguments must not be interpreted according to the EFI spec. Here, the status
+													  ; provided is EFI_SUCCESS, and upon relying on the last parameters not being
+													  ; interpreted the last 2 arguments are not initialized.
 
-
-; 56 5A 64 86 00 0A 00 00 00 00 00 00 6A 00 EB 08 00 10 00 00 00 00 00 00 6A 00 EB 04 00 00 00 00 6A 00 EB 06 00 00 00 00 EB E2 6A 02 48 8B 42 58 59 5A 41 58 41 59 FF 60 68
-
+; 56 5A 64 86 00 0A 00 00 00 00 00 00 6A 02 EB 08 00 10 00 00 00 00 00 00 59 90 EB 04 00 00 00 00 31 D2 EB 0A 00 00 00 00 48 8B 42 58 EB DE FF 60 68
